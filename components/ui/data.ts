5b0e971171ce59830cf975d0a499f684ff007980
Adekola Ariyo <ariyo@zebet.ng>
2023-10-29 01:45:57 +0100

    merging tailwind classes

---
+++ components/ui/data.ts
@@ -0,0 +1,33 @@
+import Home from "@/svgs/Home";
+import Analytics from "@/svgs/Analytics";
+import Revenue from "@/svgs/Revenue";
+import CRM from "@/svgs/CRM";
+import Apps from "@/svgs/Apps";
+
+export const NavLinkData = [
+  {
+    name: "Home",
+    href: "/home",
+    icon: Home,
+  },
+  {
+    name: "Analytics",
+    href: "#",
+    icon: Analytics,
+  },
+  {
+    name: "Revenue",
+    href: "#",
+    icon: Revenue,
+  },
+  {
+    name: "CRM",
+    href: "#",
+    icon: CRM,
+  },
+  {
+    name: "Apps",
+    href: "#",
+    icon: Apps,
+  },
+];
