4c43ff401ffe4801f522a2d683fc2d98cb2742e5
Adekola Ariyo <ariyo@zebet.ng>
2023-10-31 21:28:48 +0100

    configured request and response handlers

--- api/services/index.tsx
+++ api/services/index.tsx
@@ -1,11 +1,9 @@
 import Environment from "@/config/env";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import LocalStorage from "@/lib/localStorage";
 
 const baseUrl = Environment.NEXT_PUBLIC_BASE_URL;
 const frontendAPIHeader = {
   "content-type": "application/json",
-  authorization: `Bearer ${LocalStorage.get("token")}`,
 };
 
 const createRequest = (url: any) => ({ url, headers: frontendAPIHeader });
@@ -14,103 +12,19 @@ export const frontendAPI = createApi({
   reducerPath: "frontendAPI",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    register: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/signup",
-        method: "POST",
-        body: credentials,
-      }),
+    getUser: builder.query({
+      query: () => createRequest("/user"),
     }),
 
-    verifyPhone: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/phone-verification",
-        method: "POST",
-        body: credentials,
-      }),
+    getWallet: builder.query({
+      query: () => createRequest("/wallet"),
     }),
 
-    setPassword: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/set-password",
-        method: "POST",
-        body: credentials,
-      }),
-    }),
-
-    login: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/login",
-        method: "POST",
-        body: credentials,
-      }),
-    }),
-
-    initializeResetPassword: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/initialize-reset-password",
-        method: "POST",
-        body: credentials,
-      }),
-    }),
-
-    resetPassword: builder.mutation({
-      query: (credentials) => ({
-        url: "/users/reset-password",
-        method: "POST",
-        body: credentials,
-      }),
-    }),
-
-    // subjects
-
-    getAllSubjects: builder.query({
-      query: (params) =>
-        createRequest(`/subjects?return_only=${params.returnOnly}`),
-    }),
-
-    getSubjectById: builder.query({
-      query: (subjectId) => createRequest(`/subjects/${subjectId}`),
-    }),
-
-    //  modules
-    getAllModules: builder.query({
-      query: (params) =>
-        createRequest(
-          `/modules?return_only=${params.returnOnly}&class=${params.class}`
-        ),
-    }),
-
-    // Lessons
-
-    getSpecificLesson: builder.query({
-      query: (lessonId) => createRequest(`/lessons/${lessonId}`),
-    }),
-
-    // classes
-
-    getAllClasses: builder.query({
-      query: (params) =>
-        createRequest(`/classes?return_only=${params.readOnly}`), // name,grade,query
-    }),
-
-    getSpecificClass: builder.query({
-      query: (classId) => createRequest(`/classes/${classId}`),
+    getTransactions: builder.query({
+      query: (params) => createRequest("/transactions"),
     }),
   }),
 });
 
-export const {
-  useRegisterMutation,
-  useVerifyPhoneMutation,
-  useSetPasswordMutation,
-  useLoginMutation,
-  useInitializeResetPasswordMutation,
-  useResetPasswordMutation,
-  useGetAllSubjectsQuery,
-  useGetSubjectByIdQuery,
-  useGetAllModulesQuery,
-  useGetSpecificLessonQuery,
-  useGetAllClassesQuery,
-  useGetSpecificClassQuery,
-} = frontendAPI;
+export const { useGetUserQuery, useGetTransactionsQuery, useGetWalletQuery } =
+  frontendAPI;
